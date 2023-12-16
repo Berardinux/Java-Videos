881ee1d7d6d50910c80fffad73d032267e7c6128
Berardinux <berardinux@gmail.com>
2023-12-15 22:36:47 -0500

    Dec 15, 22:36

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("The quick brown fox jumpe over the lazy dogs");
+        number2(); 
+    }
+    public static void number2() {
+        System.out.println("This is number 2!!");
+    }
+}
